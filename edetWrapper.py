07a077696c6fbc16515bdffdfe74b4f7a095495b
GalaxyHobo <lance.bays@gmail.com>
2020-04-02 01:49:42 -0700

    feat: change path to exe to command line argument
    Other misc changes to format, code cleanup

--- edetWrapper.py
+++ edetWrapper.py
@@ -2,22 +2,24 @@
 EDET wrapper
 Executes and plots results for an EDET run
 
-NOTE! Type: "%matplotlib auto" in iPython console to 
-switch to interactive plots, or "%matplotlib inline" 
-to switch to inline, in the console.
+NOTE! Type: "%matplotlib auto" in iPython console to switch to interactive 
+plots, or "%matplotlib inline" to switch to inline, in the console.
 
-Lance Bays
-12/2/2017
+The path to the directory with the edet.exe must be provided on the command
+line. The input file (edet.in) will be written there, the program run, and
+the output file (edet.out) written there.
+    
+Lance Bays   veranautics@gmail.com
 '''
 import os
 import matplotlib.pyplot as plt 
 import numpy as np 
 import itertools
+import argparse
 # Define intertools cycle of plot symbols:
 marker = itertools.cycle(('o', 'v', 's', '<', '^', '>', '+', 'x', 'h', '*'))
 
 # Start Changing Inputs Here ************************
-
 edetInput = {'acProject': '737-700',
 'sRefInFt2': 1341.15, 
 'arWing': 9.45,
@@ -42,8 +44,8 @@ edetInput = {'acProject': '737-700',
 'crudFactor': 0.06, # CR recommends 0.28
 
 # REFERENCE CONDITIONS FOR POLARS 
-'refAltInFt': 33000.0,
-'refMach': 0.775,
+'refAltInFt': 37000.0,
+'refMach': 0.78,
 
 # VERTICAL TAIL
 'sWetVtailInFt2': 522.82,
@@ -63,21 +65,29 @@ edetInput = {'acProject': '737-700',
 'tcMisc1InDecimal': 0.02,
 'miscDeltaCdMisc1': 0.0000
 
-# BE SURE TO WRITE ANY ADDITIONAL COMPONENTS TO INPUT FILE HERE
+# BE SURE TO ADD ANY ADDITIONAL COMPONENTS HERE, AND MODIFY THE CODE BELOW 
+# TO WRITE THOSE COMPONENTS TO THE INPUT FILE. 
 }
-    
-# Establish working directory with exe...
-# Copy & paste absolute path on Local machine here within double quotes
-userExePath = "C:\AeroProgs\EDET\EDET"
 
+# OTHER PROGRAM CONSTANTS:
 # Select number of last (highest) Machs to ship in polar plots
 # (usually the last one or two look funky and skew the x-axes) 
 numLastMachSkip = 1
+# Font sizes
+axisLabelSize = 14
+titleSize = 12
+reLegendFont = 8
 
-# Stop Changing Inputs Here ********************************************
+# Stop Changing Inputs & Constants Here ***************************************
 
+# Initiate the parser
+parser = argparse.ArgumentParser(description='Runs EDET and plots output')
+# Add argument for establish working directory with exe..
+parser.add_argument('userExePath', help='Path to run directory')
+# Read arguments from the command line
+args = parser.parse_args()
 # Split drive Letter from path
-drive, exePath = userExePath.split("\\",1)
+drive, exePath = args.userExePath.split("/",1)
 
 # Handle case where user doesn't include drive in path—
 # we will assume it's on the C drive 
@@ -96,7 +106,7 @@ fin.write(str(edetInput['sRefInFt2']) + ' ' +
     str(edetInput['sweepWingQtrChrdInDeg']) + '  ' +
     str(edetInput['taperWingInDecimal']) + '\n')
 # Write Line 3 inputs
-fin.write('* S_WET CAMBER% AITEK UP_LAM% LW_LAM% DELCD0\n') 
+fin.write('* S_WET CAMBER% AITEK UP_LAM% LW_LAM% DELCD0\n')
 fin.write(str(edetInput['swetWingInFt2']) + ' '+
     str(edetInput['camberWingInPct']) + ' ' +
     str(edetInput['aiteklConv2Scrit']) + ' ' +
@@ -121,7 +131,7 @@ fin.write(str(edetInput['refAltInFt']) + ' ' +
           str(edetInput['refMach']) + '\n') 
 fin.write('*\n')
 # VERTICAL TAIL
-fin.write('*Additional component: V-Tail\n') 
+fin.write('*Additional component: V-Tail\n')
 fin.write('Vtail\n')
 fin.write('*SWET LENGTH TC_OR_FR DELCD0\n')
 fin.write(str(edetInput['sWetVtailInFt2']) + ' ' +
@@ -153,20 +163,20 @@ fin.close()
 
 ###############################################################
 # Run program
-
-#Command-Line instructions to change drivej directory, and run 
-runString = drive + " && cd \\" + exePath + " && edet.exe" 
+# Command-Line instructions to change drive, directory, and run 
+runString = drive + " && cd " + drive + "/" + exePath + " && edet.exe" 
 os.system(runString)
 
 ###############################################################
 # Read output file
-fout = open(drive + "\\" + exePath + "\\EDET.out", 'r') 
-lines=fout. readlines()
-fout.close()
+fin = open(drive + "/" + exePath + "/edet.out" , 'r')
+lines=fin.readlines()
+fin.close()
 
 ###############################################################
 # Parse results
 fig = plt.figure()
+
 # Set reasonable size for when plots appear inline 
 fig.set_figheight(15)
 fig.set_figwidth(20)
@@ -183,116 +193,120 @@ for index, line in enumerate(polarList):
 polarData=np.array(polarData)
 # Determine array of unique Mach numbers for plot
 plotMachs = np.unique(polarData[0:,0:1])
+
 # Add subplot for polars
-p1 = fig.add_subplot(3,2,1)
+p1 = fig.add_subplot(2,3,1)
 for index in plotMachs[:-numLastMachSkip]:
-	p1.plot(polarData[polarData[:,0]==index]
-	[:,np.array([False,False,False,True])],polarData[polarData[:,0]==index]
-	[:,np.array([False,False,True,False])],marker=next(marker), label=index)
-p1.legend(title='Mach', ncol=2, prop={'size': 10}) 
+    p1.plot(polarData[polarData[:,0]==index]
+      [:,np.array([False,False,False,True])], polarData[polarData[:,0]==index]
+      [:,np.array([False,False,True,False])], marker=next(marker), label=index)
+p1.legend(title='Mach', ncol=2, prop={'size': 10}).draggable() 
 p1.grid()
 p1.set(ylabel='$C_L$',
-	xlabel='$C_D$',
-	title=edetInput['acProject'] + ' Drag Polars (Ref M='+ 
-   str(edetInput['refMach']) + ', Alt=' +
-	str(int(edetInput['refAltInFt'])) + ' ft)')
-p1.xaxis.label.set_size(18)
-p1.yaxis.label.set_size(18)
-
+    xlabel='$C_D$',
+    title=edetInput['acProject'] + ' Drag Polars (Ref M='+ 
+    str(edetInput['refMach']) + ', Alt=' +
+    str(int(edetInput['refAltInFt'])) + ' ft)')
+p1.title.set_size(titleSize)
+p1.xaxis.label.set_size(axisLabelSize)
+p1.yaxis.label.set_size(axisLabelSize)
 
 # ******* Plot AOA
-p2 = fig.add_subplot(3,2,2)
+p1 = fig.add_subplot(2,3,2)
 for index in plotMachs[:-numLastMachSkip]:
-	p2.plot(polarData[polarData[:,0]==index]
-	[:,np.array([False,True,False,False])],polarData[polarData[:,0]==index]
-	[:,np.array([False,False,True,False])],marker=next(marker), label=index)
-p2.legend(title='Mach', ncol=2, prop={'size': 10}) 
-p2.grid()
-p2.set(ylabel='$C_L$',
-	xlabel=r'$\alpha$' + ' (deg)',
-	title=edetInput['acProject'] + ' AOA')
-p2.xaxis.label.set_size(18)
-p2.yaxis.label.set_size(18)
-
-
-# ******** Plot zero lift drag 
-dragBuffetList=lines[lines.index(" *    MACH       CDF       CDC BUFFET_CL\n")+
-	1:lines.index(" * INDUCED DRAG\n") - 1]
-# Convert to numpy array
-dragBuffetData=[]
-for index, line in enumerate(dragBuffetList):
-	dragBuffetData.append(np.array(list(map(float,
-	dragBuffetList[index].split()))))
-dragBuffetData=np.array(dragBuffetData)
-p3 = fig.add_subplot(3,2,3)
-p3.plot(dragBuffetData[:,0], dragBuffetData[:,1], '-o', label='$C_{Df}$') 
-p3.plot(dragBuffetData[:,0], dragBuffetData[:,2], '-v', label='$C_{Dc}$') 
-p3.plot(dragBuffetData[:,0], dragBuffetData[:,1] + dragBuffetData[:,2], '-s',
-	label='$C_{DO}$')
-p3.legend(prop={'size': 16}) 
-p3.grid()
-p3.set(ylabel='$C_D$',
-	xlabel='$Mach$', 
-	title=edetInput['acProject'] + ' Zero Lift Drag') 
-p3.xaxis.label.set_size(18)
-p3.yaxis.label.set_size(18)
-
-
-# ************ Plot buffet onset
-p4 = fig.add_subplot(3,2,4)
-p4.plot(dragBuffetData[:,0], dragBuffetData[:,3], '-o', label='$C_{L_{BO}}$')
-p4.legend(prop={'size': 16})
-p4.grid()
-p4.set(ylabel='$C_{L_{BO}}$', 
-	xlabel='$Mach$', 
-	title=edetInput['acProject'] + ' Buffet Onset') 
-p4.xaxis.label.set_size(18)
-p4.yaxis.label.set_size(18)
-
-
-# *********** Plot L/D
-# Compute L/D. Note: this overwrites CD with L/D in fourth column
-polarData[:,3] = polarData[:,2] / polarData[:,3]
-p5 = fig.add_subplot(3,2,5)
-for index in plotMachs[:-numLastMachSkip]:
-	p5.plot(polarData[polarData[:,0]==index]
-	[:,np.array([False,False,True,False])],polarData[polarData[:,0]==index]
-	[:,np.array([False,False,False,True])],marker=next(marker), label=index)
-p5.legend(title='Mach', ncol=2, prop={'size': 10}) 
-p5.grid()
-p5.set(ylabel='$L/D$',
-	xlabel='$C_L$',
-	xlim=(0,1),
-	title=edetInput['acProject'] + ' L/D (Ref M='+ 
-   str(edetInput['refMach']) + ', Alt=' +
-	str(int(edetInput['refAltInFt'])) + ' ft)')
-p5.xaxis.label.set_size(18)
-p5.yaxis.label.set_size(18)
-
+    p1.plot(polarData[polarData[:,0]==index]
+      [:,np.array([False,True,False,False])], polarData[polarData[:,0]==index]
+      [:,np.array([False,False,True,False])], marker=next(marker), label=index)
+p1.legend(title='Mach', ncol=2, prop={'size': 10}).draggable() 
+p1.grid()
+p1.set(ylabel='$C_L$',
+    xlabel=r'$\alpha$' + ' (deg)',
+    title=edetInput['acProject'] + ' AoA')
+p1.title.set_size(titleSize)
+p1.xaxis.label.set_size(axisLabelSize)
+p1.yaxis.label.set_size(axisLabelSize)
 
 # ****** Plot Re Corrn
 reList = lines[lines.index(" * ALTITUDE     MACH  DELTA_CD\n") + 1:
-	lines.index(" *MACH         ALPHA        CL        CD\n") - 3]
+    lines.index(" *MACH         ALPHA        CL        CD\n") - 3]
 # Convert to numpy array 
 reData=[]
 for index, line in enumerate(reList):
-	reData.append(np.array(list(map(float, reList[index].split()))))
+    reData.append(np.array(list(map(float, reList[index].split()))))
 reData=np.array(reData)
 # Determine array of unique Mach numbers for plot
 reAlts = np.unique(reData[0:,0:1])
 # Add subplot for Re corrn 
-p6 = fig.add_subplot(3,2,6) 
+p1 = fig.add_subplot(2,3,4) 
 for index in reAlts:
-	p6.plot(reData[reData[:,0]==index]
-	[:,np.array([False,True,False])],reData[reData[:,0]==index]
-	[:,np.array([False,False,True])], marker=next(marker),label=int(index/1000))
-p6.legend(title='Altitude (1000 ft)', ncol=2, prop={'size': 10})
-p6.grid()
-p6.set(ylabel=r'$\Delta$$C_{D_{RE}}$', 
-	xlabel='$Mach$',
-	xlim=(0,1),
-	title = edetInput['acProject'] + ' Re# Correction (Ref M=' +
-       str(edetInput['refMach']) + ', Alt=' +
-		str(int(edetInput['refAltInFt'])) + ' ft)')
-p6.xaxis.label.set_size(18)
-p6.yaxis.label.set_size(18)
+    p1.plot(reData[reData[:,0]==index]
+        [:,np.array([False,True,False])], reData[reData[:,0]==index]
+        [:,np.array([False,False,True])], marker=next(marker),
+        label=int(index/1000))
+p1.legend(title='Altitude (1000 ft)', ncol=2,
+          prop={'size': reLegendFont}).draggable()
+p1.grid()
+p1.set(ylabel=r'$\Delta$$C_{D_{RE}}$', 
+    xlabel='$Mach$',
+    xlim=(0,1),
+    title = edetInput['acProject'] + ' Re# Correction (Ref M=' +
+    str(edetInput['refMach']) + ', Alt=' +
+    str(int(edetInput['refAltInFt'])) + ' ft)')
+p1.title.set_size(titleSize)
+p1.xaxis.label.set_size(axisLabelSize)
+p1.yaxis.label.set_size(axisLabelSize)
+
+# ******** Plot zero lift drag 
+dragBuffetList=lines[lines.index(" *    MACH       CDF       CDC BUFFET_CL\n")+
+    1:lines.index(" * INDUCED DRAG\n") - 1]
+# Convert to numpy array
+dragBuffetData=[]
+for index, line in enumerate(dragBuffetList):
+    dragBuffetData.append(np.array(list(map(float,
+    dragBuffetList[index].split()))))
+dragBuffetData=np.array(dragBuffetData)
+p1 = fig.add_subplot(2,3,5)
+p1.plot(dragBuffetData[:,0], dragBuffetData[:,1], '-o', label='$C_{Df}$') 
+p1.plot(dragBuffetData[:,0], dragBuffetData[:,2], '-v', label='$C_{Dc}$') 
+p1.plot(dragBuffetData[:,0], dragBuffetData[:,1] + dragBuffetData[:,2], '-s',
+    label='$C_{DO}$')
+p1.legend(prop={'size': axisLabelSize}).draggable() 
+p1.grid()
+p1.set(ylabel='$C_D$',
+    xlabel='$Mach$', 
+    title=edetInput['acProject'] + ' Zero Lift Drag') 
+p1.title.set_size(titleSize)
+p1.xaxis.label.set_size(axisLabelSize)
+p1.yaxis.label.set_size(axisLabelSize)
+
+# ************ Plot buffet onset
+p1 = fig.add_subplot(2,3,6)
+p1.plot(dragBuffetData[:,0], dragBuffetData[:,3], '-o', label='$C_{L_{BO}}$')
+p1.legend(prop={'size': axisLabelSize}).draggable() 
+p1.grid()
+p1.set(ylabel='$C_{L_{BO}}$', 
+    xlabel='$Mach$', 
+    title=edetInput['acProject'] + ' Buffet Onset') 
+p1.title.set_size(titleSize)
+p1.xaxis.label.set_size(axisLabelSize)
+p1.yaxis.label.set_size(axisLabelSize)
+
+# *********** Plot L/D
+# Compute L/D. Note: this overwrites CD with L/D in fourth column
+polarData[:,3] = polarData[:,2] / polarData[:,3]
+p1 = fig.add_subplot(2,3,3)
+for index in plotMachs[:-numLastMachSkip]:
+    p1.plot(polarData[polarData[:,0]==index]
+        [:,np.array([False,False,True,False])],polarData[polarData[:,0]==index]
+        [:,np.array([False,False,False,True])],marker=next(marker), label=index)
+p1.legend(title='Mach', ncol=2, prop={'size': 10}).draggable()  
+p1.grid()
+p1.set(ylabel='$L/D$',
+    xlabel='$C_L$',
+    xlim=(0,1),
+    title=edetInput['acProject'] + ' L/D (Ref M='+ 
+    str(edetInput['refMach']) + ', Alt=' +
+    str(int(edetInput['refAltInFt'])) + ' ft)')
+p1.title.set_size(titleSize)
+p1.xaxis.label.set_size(axisLabelSize)
+p1.yaxis.label.set_size(axisLabelSize)
